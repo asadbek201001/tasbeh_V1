b72eb94a21322117c03a21573682f3f98e03654f
AsadbekSWE <asadbekgulomov2010@gmail.com>
2025-01-13 22:53:53 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+// console.log(hello); // undefined
+// var hello = "Salom World!";
+// console.log(hello); // 
+
+// console.log(hello); // ?
+// let hello = "Salom World!";
+// console.log(hello); // ?
+
+// const hello;
+// hello = 'salam';
+// console.log(hello); // ?
+// //////////////////////////////////////////////////////////////////
+//                         // var 
+
+// console.log(name); // undefined (hoisting!)
+// var name = "Adam";
+// console.log(name); // Adam
+
+//                             // let
+// if (true) {
+//     let age = 15;
+//     console.log(age); // 15
+//   }
+//   console.log(age); // ReferenceError: age is not defined
+
+//                             // const
+
+//   const pi = 3.14;
+// console.log(pi); // 3.14
+
+// pi = 3.14159; // TypeError: Assignment to constant variable
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
